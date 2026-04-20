2d7ba6308b91b04fdf8e0034767b5f0783d672a8
madridroyalpalace-oss <madridroyalpalace@gmail.com>
2026-04-20 22:28:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const elements = document.querySelectorAll('.reveal');
+const elements = document.querySelectorAll('.section');
 
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      entry.target.classList.add('active');
+      entry.target.style.opacity = 1;
+      entry.target.style.transform = "translateY(0)";
     }
   });
 });
 
-elements.forEach(el => observer.observe(el));
+elements.forEach(el => {
+  el.style.opacity = 0;
+  el.style.transform = "translateY(40px)";
+  el.style.transition = "1s";
+  observer.observe(el);
+});
