9d23cb56fdda7ff3bf1fcabdb7469f43220783bd
madridroyalpalace-oss <madridroyalpalace@gmail.com>
2026-04-20 22:28:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const elements = document.querySelectorAll('.reveal');
 
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active');
+    }
+  });
+});
+
+elements.forEach(el => observer.observe(el));
